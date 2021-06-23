4aae4fa2791ea9531972512996c87ecd651c698c
Pedro Henrique Ferreira Figueiredo <pedro.figueiredo@castgroup.com.br>
2021-06-22 22:54:01 -0300

    Tela de login, reset de senha e ação de login

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:iesbgram/app/constants.dart';
 import 'package:iesbgram/app/modules/home/home_module.dart';
@@ -10,30 +12,54 @@ import 'modules/login/login_module.dart';
 class AppModule extends Module {
 
   SharedPreferences _sharedPreferences;
-  AppModule(this._sharedPreferences);
+  FirebaseApp _firebaseApp;
+  AppModule(this._sharedPreferences, this._firebaseApp);
 
   @override
   List<Bind> get binds => [
     Bind.singleton((i) => _sharedPreferences),
+    Bind.instance(_firebaseApp),
+    Bind.factory((i) => FirebaseAuth.instance),
   ];
 
   @override
   late final List<ModularRoute> routes = [
     ModuleRoute(Modular.initialRoute, module: _initialModule()),
-    ModuleRoute(Constants.Routes.HOME, module: HomeModule()),
+    ModuleRoute(Constants.Routes.HOME, module: HomeModule(), guards: [_FirebaseAuthGuard()]),
     ModuleRoute(Constants.Routes.ONBOARDING, module: OnboardingModule()),
     ModuleRoute(Constants.Routes.REGISTER, module: RegisterModule(), transition: TransitionType.rotate),
     ModuleRoute(Constants.Routes.LOGIN, module: LoginModule(), transition: TransitionType.scale)
   ];
 
 
+
+
   Module _initialModule() {
     final onboardingDone = _sharedPreferences.getBool(Constants.SPK_ONBOARDING_DONE) ?? false;
+    final registerDone = _sharedPreferences.getBool(Constants.SPK_REGISTER_DONE) ?? false;
     if (onboardingDone) {
-      return RegisterModule();
+      if (registerDone) {
+        if (FirebaseAuth.instance.currentUser?.isAnonymous ?? true) {
+          return LoginModule();
+        }else {
+          return HomeModule();
+        }
+      }else {
+        return RegisterModule();
+      }
     }else {
       return OnboardingModule();
     }
   }
 
+}
+
+class _FirebaseAuthGuard extends RouteGuard {
+
+  _FirebaseAuthGuard() : super(null);
+
+  @override
+  Future<bool> canActivate(String path, ModularRoute router) {
+    return Future.value(!(FirebaseAuth.instance.currentUser?.isAnonymous ?? true));
+  }
 }
--- lib/app/modules/register/register_page.dart
+++ lib/app/modules/register/register_page.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:iesbgram/app/constants.dart';
+import 'package:iesbgram/app/modules/register/register_store.dart';
+import 'package:mobx/mobx.dart';
 
 class RegisterPage extends StatefulWidget {
   final String title;
@@ -6,13 +12,15 @@ class RegisterPage extends StatefulWidget {
   @override
   RegisterPageState createState() => RegisterPageState();
 }
-class RegisterPageState extends State<RegisterPage> {
+class RegisterPageState extends ModularState<RegisterPage, RegisterStore> {
 
   late PageController _pageController;
 
   late final TextEditingController _nameController;
   late final TextEditingController _emailController;
   late final TextEditingController _passwordController;
+
+  late final ReactionDisposer _disposer;
 
   @override
   void initState() {
@@ -22,7 +30,64 @@ class RegisterPageState extends State<RegisterPage> {
     _nameController = TextEditingController();
     _emailController = TextEditingController();
     _passwordController = TextEditingController();
+
+    _disposer = when(
+      (_) => store.user != null,
+      () => Modular.to.pushReplacementNamed(Constants.Routes.HOME)
+    );
+
   }
+
+  @override
+  void dispose() {
+    _disposer();
+    super.dispose();
+  }
+
+  late final Widget _form = PageView(
+    controller: _pageController,
+    scrollDirection: Axis.vertical,
+    physics: NeverScrollableScrollPhysics(),
+    children: [
+      _FormField(
+        controller: _nameController,
+        label: 'Qual é o seu nome?',
+        showsBackButton: false,
+        onNext: () {
+          _pageController.nextPage(duration: Duration(seconds: 1), curve: Curves.easeInOut);
+        },
+        onBack: () {
+          _pageController.previousPage(duration: Duration(seconds: 1), curve: Curves.easeInOut);
+        },
+      ),
+      _FormField(
+        controller: _emailController,
+        label: 'Qual é o seu melhor e-mail?',
+        showsBackButton: true,
+        onNext: () {
+          _pageController.nextPage(duration: Duration(seconds: 1), curve: Curves.easeInOut);
+        },
+        onBack: () {
+          _pageController.previousPage(duration: Duration(seconds: 1), curve: Curves.easeInOut);
+        },
+      ),
+      _FormField(
+        controller: _passwordController,
+        label: 'Crie uma senha',
+        showsBackButton: true,
+        onNext: () {
+          store.registerUser(
+              name: _nameController.text,
+              email: _emailController.text,
+              password: _passwordController.text
+          );
+        },
+        onBack: () {
+          _pageController.previousPage(duration: Duration(seconds: 1), curve: Curves.easeInOut);
+        },
+      ),
+    ],
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -30,45 +95,21 @@ class RegisterPageState extends State<RegisterPage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: PageView(
-        controller: _pageController,
-        scrollDirection: Axis.vertical,
-        physics: NeverScrollableScrollPhysics(),
-        children: [
-          _FormField(
-            controller: _nameController,
-            label: 'Qual é o seu nome?',
-            showsBackButton: false,
-            onNext: () {
-              _pageController.nextPage(duration: Duration(seconds: 1), curve: Curves.easeInOut);
-            },
-            onBack: () {
-              _pageController.previousPage(duration: Duration(seconds: 1), curve: Curves.easeInOut);
-            },
-          ),
-          _FormField(
-            controller: _emailController,
-            label: 'Qual é o seu melhor e-mail?',
-            showsBackButton: true,
-            onNext: () {
-              _pageController.nextPage(duration: Duration(seconds: 1), curve: Curves.easeInOut);
-            },
-            onBack: () {
-              _pageController.previousPage(duration: Duration(seconds: 1), curve: Curves.easeInOut);
-            },
-          ),
-          _FormField(
-            controller: _passwordController,
-            label: 'Crie uma senha',
-            showsBackButton: true,
-            onNext: () {
-
-            },
-            onBack: () {
-              _pageController.previousPage(duration: Duration(seconds: 1), curve: Curves.easeInOut);
-            },
-          ),
-        ],
+      body: Observer(
+        builder: (_) {
+          if (store.loading) {
+            return Column(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                CircularProgressIndicator(),
+                Text('Aguarde... salvando seu cadastro...')
+              ],
+            );
+          }
+          return _form;
+        },
       ),
     );
   }
